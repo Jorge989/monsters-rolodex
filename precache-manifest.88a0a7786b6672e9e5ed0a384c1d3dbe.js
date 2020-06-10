0aed31eb4e0619ebbfbd36bf8dc482c1afa301cc
Jorge989 <54215624+Jorge989@users.noreply.github.com>
2020-06-10 13:38:04 -0300

    Updates

---
+++ precache-manifest.88a0a7786b6672e9e5ed0a384c1d3dbe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69b6736f7a0bb86d49b2aae8fb433d04",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "6d94c14be3a5a808ffe0",
+    "url": "/monsters-rolodex/static/css/main.aa9e066f.chunk.css"
+  },
+  {
+    "revision": "8eefb47c5e7c6f1166f6",
+    "url": "/monsters-rolodex/static/js/2.27033893.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.27033893.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d94c14be3a5a808ffe0",
+    "url": "/monsters-rolodex/static/js/main.db8d8859.chunk.js"
+  },
+  {
+    "revision": "6062e4a584c29c6e0372",
+    "url": "/monsters-rolodex/static/js/runtime-main.e3d252a9.js"
+  }
+]);
