56338adac1d9808790a7d1683fec663b9a243f3f
Jorge989 <54215624+Jorge989@users.noreply.github.com>
2020-06-10 12:29:37 -0300

    adding files for github pages

--- src/App.js
+++ src/App.js
@@ -14,10 +14,7 @@ class App extends Component{
   }
 
   componentDidMount() {
-    fetch('https://jsonplaceholder.typicode.com/users',{  headers: {
- 
-      'Access-Control-Allow-Origin': '*',
-    }})
+    fetch('https://jsonplaceholder.typicode.com/users')
     .then(response => response.json())
     .then(users => this.setState({monsters: users }));
     
